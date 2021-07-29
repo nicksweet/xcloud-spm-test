d47f2972655d75990588d7f3e01254fcdaa84058
Nick Sweet <nasweet@gmail.com>
2021-07-29 09:07:14 -0700

    init

---
+++ xc-spm-test/xc_spm_test.h
@@ -0,0 +1,18 @@
+//
+//  xc_spm_test.h
+//  xc-spm-test
+//
+//  Created by Nicholas Sweet on 7/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xc_spm_test.
+FOUNDATION_EXPORT double xc_spm_testVersionNumber;
+
+//! Project version string for xc_spm_test.
+FOUNDATION_EXPORT const unsigned char xc_spm_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xc_spm_test/PublicHeader.h>
+
+
